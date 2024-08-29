e46991e952d730c08ef520faed591f3ef5675789
zeus <136583387+zeusssz@users.noreply.github.com>
2024-08-29 19:05:00 +0530

    Update snake.asm

--- snake.asm
+++ snake.asm
@@ -1,1 +1,34 @@
-BITS 16 ORG 0x100 mov ax, 0xB800 mov es, ax xor di, di .loop: mov ah, 1 int 16h test ah, ah jz .no_key mov al, ah cmp al, 0x48 je .up cmp al, 0x50 je .down cmp al, 0x4B je .left cmp al, 0x4D je .right jmp .loop .no_key: mov byte [es:di], 'O' jmp .loop .up: sub di, 160 jmp .loop .down: add di, 160 jmp .loop .left: dec di jmp .loop .right: inc di jmp .loop
+BITS 16
+ORG 0x100
+mov ax,0xB800
+mov es,ax
+xor di,di
+.loop:
+    mov ah,1
+    int 16h
+    test ah,ah
+    jz .no_key
+    cmp ah,0x48
+    je .up
+    cmp ah,0x50
+    je .down
+    cmp ah,0x4B
+    je .left
+    cmp ah,0x4D
+    je .right
+    jmp .loop
+.no_key:
+    mov byte [es:di],'O'
+    jmp .loop
+.up:
+    sub di,160
+    jmp .loop
+.down:
+    add di,160
+    jmp .loop
+.left:
+    dec di
+    jmp .loop
+.right:
+    inc di
+    jmp .loop
