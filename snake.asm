11f7c0412f26e864e181d7c1e31e4fdce4dbb675
zeus <136583387+zeusssz@users.noreply.github.com>
2024-08-29 18:39:36 +0530

    Update snake.asm

--- snake.asm
+++ snake.asm
@@ -1,23 +1,48 @@
 BITS 16
 ORG 0x100
 
-mov ah, 00h
-int 16h
-mov ax, 0B800h
-mov es, ax
-mov di, 400h
-mov cx, 0302h
-next:
-mov [es:di], cl
-inc di
-loop next
-wait:
-mov ah, 01h
-int 16h
-jz wait
+mov ax, 0B800h      
+mov es, ax          
+mov di, 401h        
+mov byte [es:di], 'O'   
+mov byte [es:di+2], 'X' 
+
+mov ah, 1           
+waitkey:
+int 16h             
+jz move             
+int 16h             
+cmp ah, 48h         
+je up               
+cmp ah, 50h         
+je down             
+cmp ah, 4Dh         
+je right            
+cmp ah, 4Bh         
+je left             
+jmp waitkey         
+
+move:              
+cmp byte [es:di], 'X'
+je game_over        
+mov byte [es:di], ' '  
+mov [es:di], 'O'       
+mov byte [es:di+2], 'X' 
+jmp waitkey          
+
+up:
+sub di, 160
+jmp move
+down:
+add di, 160
+jmp move
+left:
+sub di, 2
+jmp move
+right:
+add di, 2
+jmp move
+
+game_over:
 mov ah, 0
 int 16h
-dec byte [es:di]
-jnz next
-inc di
-jmp wait
