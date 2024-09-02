167bed250c1cba9b11aaa7b556032f2de8e11b32
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-02 16:59:50 +0530

    Update snake.asm

--- snake.asm
+++ snake.asm
@@ -9,19 +9,20 @@ xor di, di
     mov ah, 1
     int 16h
     jz .draw
-    cmp ah, 0x48     ; Up arrow key
-    je .move_up
-    cmp ah, 0x50     ; Down arrow key
-    je .move_down
-    cmp ah, 0x4B     ; Left arrow key
-    je .move_left
-    cmp ah, 0x4D     ; Right arrow key
-    je .move_right
+    sub ah, 0x48
+    js .loop 
+    jz .up
+    dec ah
+    jz .down 
+    dec ah
+    jz .left 
+    dec ah
+    jz .right 
     jmp .loop
 
-.move_up:    sub di, 160
-.move_down:  add di, 160
-.move_left:  dec di
-.move_right: inc di
-.draw:       mov [es:di], byte 'O'
-             jmp .loop
+.up:    sub di, 160
+.down:  add di, 160
+.left:  dec di
+.right: inc di
+.draw:  mov [es:di], byte 'O'
+        jmp .loop
