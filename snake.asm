423c839261ba8ac87023bd2b49dba379f55323c8
zeus <136583387+zeusssz@users.noreply.github.com>
2024-09-02 17:01:48 +0530

    Update snake.asm

--- snake.asm
+++ snake.asm
@@ -9,20 +9,20 @@ xor di, di
     mov ah, 1
     int 16h
     jz .draw
-    sub ah, 0x48
-    js .loop 
-    jz .up
-    dec ah
-    jz .down 
-    dec ah
-    jz .left 
-    dec ah
-    jz .right 
-    jmp .loop
+    sub ah, 0x47
+    cmp ah, 4
+    ja .loop
+    jmp word [tbl + ah*2]
 
 .up:    sub di, 160
+        jmp .loop
 .down:  add di, 160
+        jmp .loop
 .left:  dec di
+        jmp .loop
 .right: inc di
+        jmp .loop
 .draw:  mov [es:di], byte 'O'
         jmp .loop
+
+tbl:    dw .up, .left, .down, .right
